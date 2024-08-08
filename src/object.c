3e60013688d4ebebafca1576356562eaa4f22177
aarol <aarol@iki.fi>
2024-08-08 20:39:46 +0300

    global variables

--- src/object.c
+++ src/object.c
@@ -50,7 +50,7 @@ ObjString* takeString(char* chars, int length) {
 
 ObjString* copyString(const char* chars, int length) {
   uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindStrng(&vm.strings, chars, length, hash);
+  ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
   if (interned != NULL) return interned;
 
   char* heapChars = ALLOCATE(char, length + 1);
